04da0b3ee0d27c3a85001b4d6388878f884dda0e
Sabyasachi Mohanty <sabyasachim999@gmail.com>
2025-01-11 22:31:02 +0530

    Vercel routing fixes

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Canvas from "./components/Canvas"
 import HomePage from "./components/HomePage"
+import NotFound from './components/NotFound'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/canvas/:id" element={<Canvas />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
       
---
+++ fe/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (  
+    <div className='h-screen w-screen flex justify-center items-center bg-black'>
+        <div className='font-bold text-8xl text-green-400'>404 NotFound</div>
+    </div>
+  )
+}
+
+export default NotFound
