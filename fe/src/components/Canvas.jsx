72a825bddb5be927da031744950d97a7ccd2830b
Sabyasachi Mohanty <sabyasachim999@gmail.com>
2025-01-11 17:56:54 +0530

    Commit

--- fe/src/components/Canvas.jsx
+++ fe/src/components/Canvas.jsx
@@ -19,7 +19,7 @@ const Canvas = () => {
   const [isDragging, setIsDragging] = useState(false)
 
   useEffect(() => {
-    socketRef.current = new WebSocket('wss://collabcanvasapi.sabya.site')
+    socketRef.current = new WebSocket('https://collabcanvasapi.sabya.site')
 
     socketRef.current.addEventListener('open', () => {
       console.log("Websocket connection established")
